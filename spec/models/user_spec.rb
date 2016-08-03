1464985682e177e0c83689a47f84a0c56369a4d0
annikoff <annikoff@ya.ru>
2016-08-03 22:41:08 +0300

    add: specs for models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:comments) }
+  end
+end
