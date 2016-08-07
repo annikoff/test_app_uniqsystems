ded8d55f85dfa205cc80ce59de265a418c331a07
annikoff <annikoff@ya.ru>
2016-08-07 17:45:03 +0300

    add: mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,15 @@ class User < ApplicationRecord
   validates_uniqueness_of :name, :email
   has_many :comments
   before_create :set_is_admin
+  after_create :welcome_message
 
   def set_is_admin
     self.is_admin = self.class.all.size.zero?
+  end
+
+  private
+
+  def welcome_message
+    UserMailer.welcome_message(self).deliver
   end
 end
