f6f74432015a400b6c11510cd4459e6bfbb7d66b
annikoff <annikoff@ya.ru>
2016-08-07 16:59:18 +0300

    add: comments specs, and change: user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, :type => :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
 
   describe 'associations' do
