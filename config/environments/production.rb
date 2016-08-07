563745d659d11d57f95af27a6002200f30ced459
annikoff <annikoff@ya.ru>
2016-08-08 01:14:57 +0300

    update mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: ENV['SMTP_DOMAIN'],
       port: 587,
-      authentication: 'plain',
+      authentication: 'login',
       user_name: ENV['SMTP_USER'],
       password: ENV['SMTP_PASSWORD'],
       enable_starttls_auto: true
