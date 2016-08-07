119b170a914626923551d349057c08aab9af6ed3
annikoff <annikoff@ya.ru>
2016-08-08 01:09:48 +0300

    update mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: ENV['SMTP_DOMAIN'],
@@ -67,7 +68,7 @@ Rails.application.configure do
       authentication: 'plain',
       user_name: ENV['SMTP_USER'],
       password: ENV['SMTP_PASSWORD'],
-      enable_starttls_auto: false
+      enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
